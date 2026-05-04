da41774eaa005e2f749867550190a24f33a88376
kadirturan <aturan446@gmail.com>
2026-05-04 18:18:01 +0300

    feat: Discount.Grpc servisi eklendi, Basket.API Discount.Grpc ile iletişimi kuruldu. Docker-Compose güncellendi. Ordering.API oluşturuldu.

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,12 +1,17 @@
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 
 var builder = WebApplication.CreateBuilder(args);
-var assembly = typeof(Program).Assembly;
-var dbConnectionString = builder.Configuration.GetConnectionString("MartenDb")!;
-var redisConnectionString = builder.Configuration.GetConnectionString("Redis")!;
 
+#region Variables
+var assembly = typeof(Program).Assembly;
+var dbConnectionString = builder.Configuration.GetConnectionString("Database")!;
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis")!;
+#endregion
+
+#region ApplicationServices
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(assembly);
@@ -14,34 +19,58 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+builder.Services.AddCarter();
+#endregion
+
+#region DataServices
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(dbConnectionString);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
-builder.Services.AddCarter();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
-builder.Services.Decorate<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 builder.Services.AddStackExchangeRedisCache(opts =>
 {
     opts.Configuration = redisConnectionString;
     //opts.InstanceName = "Basket";
 });
+#endregion
+
+#region Cross-Cutting Services
 builder.Services.AddHealthChecks()
     .AddNpgSql(dbConnectionString)
     .AddRedis(redisConnectionString);
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+#endregion
+
+#region Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opts =>
+{
+    opts.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
+});
+#endregion
+
+#region Pipeline
 var app = builder.Build();
 app.MapCarter();
-app.UseExceptionHandler();
+app.UseExceptionHandler(opts => { });
 app.UseHealthChecks("/health",
     new HealthCheckOptions
     {
         ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
     });
 app.Run();
-
+#endregion
 
---
+++ Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder application)
+        {
+            using var scope = application.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            dbContext.Database.MigrateAsync();
+            return application;
+        }
+    }
+}
