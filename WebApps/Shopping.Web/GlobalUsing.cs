688f4fecb508d9106d0412a14aa1f115c1d8eb22
kadirturan <aturan446@gmail.com>
2026-05-24 18:33:05 +0300

    Shopping.Web web uygulamasini eklendi ve Docker entegrasyonunu yapıldı
    - Shopping.Web Razor Pages projesi ve statik varliklari eklendi
    - Docker Compose yapilandirmasina yeni servis eklendi
    - Launch ayarlari guncellendi

---
+++ WebApps/Shopping.Web/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Shopping.Web.Models.Ordering;
+global using Refit;
+global using Shopping.Web.Services;
+global using Shopping.Web.Models.Catalog;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
